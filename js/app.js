5fe9bd43ab570b428d4c7fb865e5ad8c581fef81
timothy <timothy@zipcoders-MacBook-Pro.local>
2017-12-06 11:42:11 -0500

    Saving

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 var Dice = (function () {
     function Dice(sides) {
         this.sides = sides;
@@ -68,4 +74,156 @@ var User = (function () {
     });
     return User;
 }());
+/// <reference path="Gamble.ts"/>
+/// <reference path="Game.ts"/>
+var CrapPointPair = (function () {
+    function CrapPointPair(a, b, text) {
+        this.a = a;
+        this.b = b;
+        this.text = text;
+    }
+    CrapPointPair.prototype.isInPair = function (passed) {
+        return (this.a == passed || this.b == passed);
+    };
+    return CrapPointPair;
+}());
+var Craps = (function () {
+    function Craps() {
+        this.pointPairs = [new CrapPointPair(6, 8, "6-8"),
+            new CrapPointPair(5, 9, "5-9"),
+            new CrapPointPair(10, 4, "10-4")];
+        this.boolean = play(String, userInput);
+        var twoToTwelve;
+        for (var i = 2; i < 13; i++) {
+            twoToTwelve.push(i);
+        }
+        this.dice = new Dice(twoToTwelve);
+        this.mainPot = new MoneyContainer();
+        this.sidePot = new MoneyContainer();
+        this.numberRolled = 0;
+        this.point = 0;
+        this.determineFirstRoller();
+    }
+    //Add to pot
+    Craps.prototype.takeBet = function (bet) {
+        this.mainPot.addMoney(bet);
+    };
+    Craps.prototype.takeSideBet = function (bet) {
+        this.sidePot.addMoney(bet);
+    };
+    //remove some from pot
+    Craps.prototype.settleBet = function (winnings) {
+        return (this.mainPot.takeOutMoney(winnings));
+    };
+    Craps.prototype.settleSideBet = function (winnings) {
+        return (this.sidePot.takeOutMoney(winnings));
+    };
+    //Take all from pot
+    Craps.prototype.emptyPot = function () {
+        return this.mainPot.takeAllMoney();
+    };
+    Craps.prototype.emptySidePot = function () {
+        return this.sidePot.takeAllMoney();
+    };
+    Craps.prototype.determineFirstRoller = function () {
+        //Player vs House, highest goes first, house wins tie
+        this.isPlayerTurn = (this.dice.rollDie() - this.dice.rollDie() > 0);
+    };
+    Craps.prototype.changePlayerTurn = function () {
+        this.isPlayerTurn = !this.isPlayerTurn;
+        this.resetTurn();
+    };
+    Craps.prototype.resetTurn = function () {
+        this.numberRolled = 0;
+        this.point = 0;
+    };
+    Craps.prototype.getPlayerTurn = function () {
+        return this.isPlayerTurn;
+    };
+    Craps.prototype.getNumberRolled = function () {
+        return this.numberRolled;
+    };
+    Craps.prototype.getSidePot = function () {
+        return this.sidePot;
+    };
+    Craps.prototype.getMainPot = function () {
+        return this.mainPot;
+    };
+    Craps.prototype.initialThrow = function () {
+        //returns -1 if 2/3/12
+        // 1 if 7/11,
+        // 0 if point set
+        this.numberRolled = this.dice.rollDie();
+        switch (this.numberRolled) {
+            case 2:
+            case 3:
+            case 12: {
+                return -1;
+            }
+            case 7:
+            case 11: {
+                return 1;
+            }
+            default: {
+                this.point = this.numberRolled;
+                for (var i = 0; i < this.pointPairs.length; i++) {
+                    if (this.pointPairs[i].isInPair(this.point)) {
+                        this.pair = this.pointPairs[i];
+                        break;
+                    }
+                }
+                return 0;
+            }
+        }
+    };
+    Craps.prototype.secondaryThrow = function () {
+        //returns 1 if point met
+        //returns 0 if nothing met
+        //returns any other number if pair met
+        this.numberRolled = this.dice.rollDie();
+        if (this.numberRolled == this.point) {
+            return 1;
+        }
+        else if (this.numberRolled == 7) {
+            return -1;
+        }
+        else if (this.pair.isInPair(this.numberRolled)) {
+            return this.numberRolled;
+        }
+        else {
+            return 0; //Neutral.
+        }
+    };
+    Craps.prototype.toString = function () {
+        var returnMe = "";
+        if (this.isPlayerTurn) {
+            returnMe += "It is your turn\n";
+        }
+        else {
+            returnMe += "It is your opponent's turn\n";
+        }
+        if (this.point == 0) {
+            returnMe += "Point has not been set, and so we do not have a pair to side bet on\n";
+        }
+        else {
+            returnMe += "Point is " + this.point + " and we are making side bets on " + this.pair.text + "\n";
+        }
+        if (this.numberRolled != 0) {
+            returnMe += "Last roll was " + this.numberRolled + "\n";
+        }
+        else {
+            returnMe += "Nobody has rolled yet\n";
+        }
+        returnMe += "Main pot is " + defaultFormat.format(mainPot.getMoney()) + "\n";
+        returnMe += "Side pot is " + defaultFormat.format(sidePot.getMoney()) + "\n";
+        return returnMe;
+    };
+    __decorate([
+        Override
+    ], Craps.prototype, "boolean", void 0);
+    return Craps;
+}());
+{
+    return ("Y".equalsIgnoreCase(userInput));
+}
 //# sourceMappingURL=app.js.map
--- ts/Craps.ts
+++ ts/Craps.ts
@@ -1,188 +1,184 @@
 /// <reference path="Gamble.ts"/>
 /// <reference path="Game.ts"/>
 
+class CrapPointPair {
+    a:number;
+    b:number;
+    text:string;
+
+    constructor(a:number, b:number, text:string) {
+        this.a = a;
+        this.b = b;
+        this.text=text;
+    }
+
+    isInPair(passed:number):boolean {
+        return (this.a==passed || this.b==passed);
+    }
+
+}
+
 class Craps implements Gamble, Game {
 
-    private NumberFormat defaultFormat;
-    private CrapPointPair pair;
-    private Dice<Integer> dice;
-    private int numberRolled;
-    private MoneyContainer mainPot;
-    private MoneyContainer sidePot;
-    private boolean isPlayerTurn;
-    private int point;
+    pointPairs:Array<CrapPointPair> =
+        [new CrapPointPair(6, 8, "6-8"),
+         new CrapPointPair(5, 9, "5-9"),
+         new CrapPointPair(10, 4, "10-4")];
+    pair:CrapPointPair;
+    dice:Dice<number>;
+    numberRolled:number;
+    mainPot:MoneyContainer;
+    sidePot:MoneyContainer;
+    isPlayerTurn:boolean;
+    point:number;
 
-    public Craps(){
-        ArrayList<Integer> twoToTwelve = new ArrayList<>();
-        for (int i=2; i<13; i++){
-            twoToTwelve.add(i);
+    constructor(){
+        let twoToTwelve:Array<number>;
+        for (let i:number=2; i<13; i++){
+            twoToTwelve.push(i);
         }
-        defaultFormat=NumberFormat.getCurrencyInstance();
-        dice = new Dice(twoToTwelve);
-        mainPot=new MoneyContainer();
-        sidePot=new MoneyContainer();
-        numberRolled=0;
-        point=0;
-        determineFirstRoller();
+        this.dice = new Dice(twoToTwelve);
+        this.mainPot=new MoneyContainer();
+        this.sidePot=new MoneyContainer();
+        this.numberRolled=0;
+        this.point=0;
+        this.determineFirstRoller();
     }
 
     //Add to pot
-    @Override
-    public void takeBet(Double bet) {
-    takeBet(bet, mainPot);
-}
-private void takeBet(Double bet, MoneyContainer moneyContainer) {
-    moneyContainer.addMoney(bet);
-}
-public void takeSideBet(Double bet){
-    takeBet(bet, sidePot);
-}
+    takeBet(bet:number):void {
+        this.mainPot.addMoney(bet);
+    }
+    takeSideBet(bet:number):void{
+        this.sidePot.addMoney(bet);
+    }
 
-//remove some from pot
-@Override
-public Double settleBet(Double winnings) {
-    return (settleBet(winnings, mainPot));
-}
-private Double settleBet(Double winnings, MoneyContainer moneyContainer){
-    return (moneyContainer.takeOutMoney(winnings));
-}
-public Double settleSideBet(Double winnings){
-    return (settleBet(winnings, sidePot));
-}
+    //remove some from pot
+    settleBet(winnings:number):number {
+        return (this.mainPot.takeOutMoney(winnings));
+    }
+    settleSideBet(winnings:number):number{
+    return (this.sidePot.takeOutMoney(winnings));
+    }
 
-//Take all from pot
-public Double emptyPot(){
-    return emptyPot(mainPot);
-}
-private Double emptyPot(MoneyContainer moneyContainer){
-    return moneyContainer.takeAllMoney();
-}
-public Double emptySidePot(){
-    return emptyPot(sidePot);
-}
+    //Take all from pot
+    emptyPot():number{
+        return this.mainPot.takeAllMoney();
+    }
+    emptySidePot():number{
+        return this.sidePot.takeAllMoney();
+    }
 
 
-public void determineFirstRoller(){
-    isPlayerTurn=(dice.rollDie()-dice.rollDie()>0);//Player vs House, highest goes first, house wins tie
-}
-public void changePlayerTurn(){
-    isPlayerTurn=!isPlayerTurn;
-    resetTurn();
-}
-public void resetTurn() {
-    numberRolled=0;
-    point=0;
-}
+    determineFirstRoller():void{
+        //Player vs House, highest goes first, house wins tie
+        this.isPlayerTurn=(this.dice.rollDie()-this.dice.rollDie()>0);
+    }
+    changePlayerTurn():void{
+        this.isPlayerTurn=!this.isPlayerTurn;
+        this.resetTurn();
+    }
+    resetTurn():void{
+        this.numberRolled=0;
+        this.point=0;
+    }
 
-public boolean getPlayerTurn(){return isPlayerTurn;}
+    getPlayerTurn():boolean{
+        return this.isPlayerTurn;
+    }
 
-public int getNumberRolled(){return numberRolled;}
-public MoneyContainer getSidePot() {
-    return sidePot;
-}
-public MoneyContainer getMainPot() {
-    return mainPot;
-}
+    getNumberRolled():number{
+        return this.numberRolled;
+    }
+    getSidePot():MoneyContainer{
+        return this.sidePot;
+    }
+    getMainPot():MoneyContainer{
+        return this.mainPot;
+    }
 
-public Integer initialThrow(){ //returns -1 if 2/3/12
-    // 1 if 7/11,
-    // 0 if point set
-    numberRolled = dice.rollDie();
+    initialThrow():number{
+        //returns -1 if 2/3/12
+        // 1 if 7/11,
+        // 0 if point set
+        this.numberRolled = this.dice.rollDie();
 
-    switch (numberRolled){
-        case  2:
-        case  3:
-        case 12:{
-            return -1;
-        }
-        case  7:
-        case 11:{
-            return 1;
-        }
-        default:{
-            point=numberRolled;
-            for (CrapPointPair p : CrapPointPair.values()){
-                if (p.isInPair(point)){
-                    pair=p;
-                }
+        switch (this.numberRolled){
+            case  2:
+            case  3:
+            case 12:{
+                return -1;
             }
-            return 0;
+            case  7:
+            case 11:{
+                return 1;
+            }
+            default:{
+                this.point=this.numberRolled;
+
+                for (let i=0; i<this.pointPairs.length; i++)
+                {
+                    if (this.pointPairs[i].isInPair(this.point)){
+                        this.pair=this.pointPairs[i];
+                        break;
+                    }
+                }
+                return 0;
+            }
+        }
+
+    }
+    secondaryThrow():number{//returns -1 if crapped out,
+        //returns 1 if point met
+        //returns 0 if nothing met
+        //returns any other number if pair met
+        this.numberRolled=this.dice.rollDie();
+
+        if (this.numberRolled==this.point){//Won round
+            return 1;
+        } else if (this.numberRolled==7)//Lost round
+        {
+            return -1;
+        } else if (this.pair.isInPair(this.numberRolled)){//Won sideBet
+            return this.numberRolled;
+        } else{
+            return 0;//Neutral.
         }
     }
 
-}
-public Integer secondaryThrow(){//returns -1 if crapped out,
-    //returns 1 if point met
-    //returns 0 if nothing met
-    //returns any other number if pair met
-    numberRolled=dice.rollDie();
+    toString():string{
 
-    if (numberRolled==point){//Won round
-        return 1;
-    } else if (numberRolled==7)//Lost round
-    {
-        return -1;
-    } else if (pair.isInPair(numberRolled)){//Won sideBet
-        return numberRolled;
-    } else{
-        return 0;//Neutral.
+        let returnMe:string="";
+
+        if (this.isPlayerTurn){
+            returnMe+="It is your turn\n";
+        } else{
+            returnMe+="It is your opponent's turn\n";
+        }
+
+        if (this.point==0){
+            returnMe+="Point has not been set, and so we do not have a pair to side bet on\n";
+        } else{
+            returnMe+="Point is "+this.point+" and we are making side bets on "+this.pair.text+"\n";
+        }
+
+        if (this.numberRolled!=0){
+            returnMe+="Last roll was "+this.numberRolled+"\n";
+        }
+        else{
+            returnMe+="Nobody has rolled yet\n";
+        }
+
+        returnMe+="Main pot is "+defaultFormat.format(mainPot.getMoney())+"\n";
+        returnMe+="Side pot is "+defaultFormat.format(sidePot.getMoney())+"\n";
+
+
+        return returnMe;
     }
-}
-
-@Override
-public String toString(){
-    String returnMe="";
-
-    if (isPlayerTurn){
-        returnMe+="It is your turn\n";
-    } else{
-        returnMe+="It is your opponent's turn\n";
+    @Override
+    public boolean play(String userInput) {
+        return ("Y".equalsIgnoreCase(userInput));
     }
-
-    if (point==0){
-        returnMe+="Point has not been set, and so we do not have a pair to side bet on\n";
-    } else{
-        returnMe+="Point is "+point+" and we are making side bets on "+pair.text+"\n";
-    }
-
-    if (numberRolled!=0){
-        returnMe+="Last roll was "+numberRolled+"\n";
-    }
-    else{
-        returnMe+="Nobody has rolled yet\n";
-    }
-
-    returnMe+="Main pot is "+defaultFormat.format(mainPot.getMoney())+"\n";
-    returnMe+="Side pot is "+defaultFormat.format(sidePot.getMoney())+"\n";
-
-
-    return returnMe;
-}
-@Override
-public boolean play(String userInput) {
-    return ("Y".equalsIgnoreCase(userInput));
-}
-
-private enum CrapPointPair {
-    sixEight(6, 8, "6-8"),
-    fiveNine(5, 9, "5-9"),
-    tenFour(10, 4, "10-4");
-
-int a;
-int b;
-String text;
-
-CrapPointPair(int a, int b, String text) {
-    this.a = a;
-    this.b = b;
-    this.text=text;
-}
-
-boolean isInPair(int passed) {
-    return (this.a==passed || this.b==passed);
-}
-
-}
 
 
 
