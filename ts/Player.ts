f3cd3289f697e3174d67b619c3b073db5fc27415
Zachary <zstimmel@gmail.com>
2017-12-06 10:41:41 -0500

    saving

---
+++ ts/Player.ts
@@ -0,0 +1,58 @@
+class Player {
+    private _name: string;
+    private _balance: number;
+    private _age: number;
+
+    //private playerHand : new Array<>();
+
+    public constructor(name: string, balance: number, age: number) {
+        this._name = name;
+        this._balance = balance;
+        this._age = age;
+    }
+
+    // getName(): string {
+    //     return this.name;
+    // }
+    //
+    // setName(newName: string): void {
+    //     this.name = newName;
+    // }
+    //
+    // getBalance(): number {
+    //     return this.balance;
+    // }
+    //
+    // setBalance(newBalance: number): void {
+    //     this.balance = newBalance;
+    // }
+
+
+    get name(): string {
+        return this._name;
+    }
+
+    set name(value: string) {
+        this._name = value;
+    }
+
+    get balance(): number {
+        return this._balance;
+    }
+
+    set balance(value: number) {
+        this._balance = value;
+    }
+
+    get age(): number {
+        return this._age;
+    }
+
+    set age(value: number) {
+        this._age = value;
+    }
+
+    addToBalance (amount : number) : void {
+        this._balance += amount;
+    }
+}
