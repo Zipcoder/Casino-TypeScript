ce00414249c4979754cb8ad3401edc21b105127f
katherine <katherine@zipcoders-MacBook-Pro.local>
2017-12-07 18:25:36 -0500

    gamble

--- ts/Gamble.ts
+++ ts/Gamble.ts
@@ -1,5 +1,6 @@
-import {Player} from './Player';
-export interface Gamble<T> {
+import {Player} from './Player'
+import {Game} from './Game'
+export interface Gamble<T extends Game<T>> {
   takeBet(player: Player<T>, amount: number);
   payOutBets();
   clearAllBets();
