159e356fc7802494266db3290986b71b63fea424
andrea <andrea@zipcoders-MacBook-Pro-4.local>
2017-12-08 10:56:26 -0500

    getting changes

--- ts/BlackJack.ts
+++ ts/BlackJack.ts
@@ -1,23 +1,35 @@
 /// <reference path="BlackJackPlayer.ts" />
 /// <reference path="CardGame.ts" />
-/// <reference path="CardPlayer.ts" />
 /// <reference path="Deck.ts" />
 
 class BlackJack extends CardGame{
     private dealer: BlackJackPlayer;
-    
+    private blackJackPlayers: BlackJackPlayer[];
+    private deck: Deck;
+
     constructor(){
         super();
         this.dealer = new BlackJackPlayer();
+        this.blackJackPlayers = [];
+        this.deck = new Deck();
+    }
+
+    addBlackJackPlayer(player: BlackJackPlayer){
+        this.blackJackPlayers.push(player);
     }
 
     getDealer(): BlackJackPlayer{
         return this.dealer;
     }
 
-    hitPlayer(blackJackPlayer: BlackJackPlayer){
-        var nextCard = deck.getTopCard();
-        blackJackPlayer.getHand().push(nextCard);
+    dealInitialCards(){
+        this.deck.shuffle(47);
+        for(var i = 0; i < 2; i++){
+            for(var j = 0; j < this.blackJackPlayers.length; j++){
+                var nextCard = this.deck.getTopCard();
+                this.blackJackPlayers[j].getHand().push(nextCard);
+            }
+        }
     }
 
     getCardPointValue(card: Card): number{
@@ -34,45 +46,19 @@ class BlackJack extends CardGame{
         }
     }
     
-    calculatePlayerScore(blackJackPlayer: CardPlayer): number{
+    calculatePlayerScore(blackJackPlayer: BlackJackPlayer): number{
         var score = 0;
         for(var i = 0; i < blackJackPlayer.getHand().length; i++){
             score += blackJack.getCardPointValue(blackJackPlayer.getHand()[i]);
         }
         return score;
     }
-
-    // play()     
-    pressPlay(){
-        //creates a blackJackGame
-        //creates a player and a dealer
-        //adds player/dealer to the gam
-    }
-    
-    askForHitOrStay(){
-
-    }
-    
-    dealerPlay(){
-    }
-    
-    isPlayerWinner(blackJackPlayer: BlackJackPlayer, dealer: BlackJackPlayer): boolean{
-        return false;
-    }
-    
-    playAgain(input: boolean): boolean{
-        return input;
-
-    }
-    
-        }
-    
-
+}
 
 var blackJack = new BlackJack();
-var blackJackPlayer = new CardPlayer();
-blackJack.addCardPlayer(blackJackPlayer);
-blackJack.deal(2);
+var blackJackPlayer = new BlackJackPlayer();
+blackJack.addBlackJackPlayer(blackJackPlayer);
+blackJack.dealInitialCards();
 var score = blackJack.calculatePlayerScore(blackJackPlayer);
 console.log(score);
 
