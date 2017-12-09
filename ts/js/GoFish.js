222af676d0ae3e53bae8d4b51734dcad591c515c
katherine <katherine@zipcoders-MBP.home>
2017-12-09 10:15:57 -0500

    go fish class finished

--- ts/js/GoFish.js
+++ ts/js/GoFish.js
@@ -15,88 +15,76 @@ define(["require", "exports", "./CardGame"], function (require, exports, CardGam
         __extends(GoFish, _super);
         function GoFish() {
             var _this = _super.call(this, 1) || this;
+            _this.MIN_NUMBER_OF_PLAYERS = 2;
+            _this.MAX_NUMBER_OF_PLAYERS = 5;
+            _this.pointValues = {
+                "TWO": 1,
+                "THREE": 2,
+                "FOUR": 3,
+                "FIVE": 4,
+                "SIX": 5,
+                "SEVEN": 6,
+                "EIGHT": 7,
+                "NINE": 8,
+                "TEN": 9,
+                "JACK": 10,
+                "QUEEN": 11,
+                "KING": 12,
+                "ACE": 13,
+            };
+            _this.numPlayers = 0;
+            _this.numInitialCards = 0;
             _this.players = [];
             return _this;
-            //this.setPointValues();
         }
+        GoFish.prototype.setNumInitialCards = function () {
+            this.numPlayers = this.getNumPlayers();
+            if (this.numPlayers >= this.MIN_NUMBER_OF_PLAYERS && this.numPlayers <= 3) {
+                this.numInitialCards = 7;
+            }
+            else {
+                this.numInitialCards = 5;
+            }
+        };
+        GoFish.prototype.dealInitialCards = function () {
+            for (var i = 0; i < this.numInitialCards; i++) {
+                for (var player in this.players) {
+                    this.players[player].addCardToHand(this.drawFromStock());
+                }
+            }
+        };
+        GoFish.prototype.playerGoFish = function (player) {
+            player.goFish(this.drawFromStock());
+        };
+        GoFish.prototype.determineWinner = function () {
+            var maxPoints = 0;
+            var winner = undefined;
+            for (var player in this.players) {
+                var goFishPlayer = this.players[player];
+                var score = 0;
+                for (var book in goFishPlayer.getBooks()) {
+                    var currentBook = goFishPlayer.getBooks()[book];
+                    var rankOfBook = currentBook.getCard(0).getFaceValue();
+                    score += this.pointValues[rankOfBook];
+                }
+                if (score > maxPoints) {
+                    winner = goFishPlayer;
+                    maxPoints = score;
+                }
+            }
+            return winner;
+        };
         GoFish.prototype.addPlayers = function (players) {
             this.players = players;
+        };
+        GoFish.prototype.getPlayers = function () {
+            return this.players;
+        };
+        GoFish.prototype.getNumPlayers = function () {
+            return this.players.length;
         };
         return GoFish;
     }(CardGame_1.CardGame));
     exports.GoFish = GoFish;
 });
-// public class GoFish extends CardGame<GoFish> {
-// 
-//     public final int MIN_NUMBER_OF_PLAYERS = 2;
-//     public final int MAX_NUMBER_OF_PLAYERS = 5;
-//     private final HashMap<Card.FaceValue, Integer> pointValues = new HashMap<Card.FaceValue, Integer>();
-//     private int numPlayers = 0;
-//     private int numInitialCards = 0;
-// 
-// 
-//     public GoFish() {
-//         super(1);
-//         setPointValues();
-//     }
-// 
-//     @Override
-//     public ArrayList<GoFishPlayer> getPlayers() {
-//         return (ArrayList<GoFishPlayer>) players;
-//     }
-// 
-//     public void setNumInitialCards() {
-//         numPlayers = getNumPlayers();
-//         if(numPlayers >= MIN_NUMBER_OF_PLAYERS && numPlayers <= 3) {
-//             numInitialCards = 7;
-//         } else {
-//             numInitialCards = 5;
-//         }
-//     }
-// 
-//     public void dealInitialCards() {
-//         for(int i = 0; i < numInitialCards; i++) {
-//             for(GoFishPlayer goFishPlayer : getPlayers()) {
-//                 goFishPlayer.addCardToHand(drawFromStock());
-//             }
-//         }
-//     }
-// 
-//     public void playerGoFish(GoFishPlayer player) {
-//         player.goFish(this.drawFromStock());
-//     }
-// 
-//     public GoFishPlayer determineWinner() {
-//         int maxPoints = 0;
-//         GoFishPlayer winner = null;
-//         for(GoFishPlayer goFishPlayer : getPlayers()) {
-//             int score = 0;
-//             for(CardPile book : goFishPlayer.getBooks()) {
-//                 score += pointValues.get(book.getCard(0).getFaceValue());
-//             }
-//             if(score > maxPoints) {
-//                 winner = goFishPlayer;
-//                 maxPoints = score;
-//             }
-//         }
-//         return winner;
-//     }
-// 
-//     @Override
-//     public void setPointValues() {
-//         pointValues.put(TWO, 1);
-//         pointValues.put(THREE, 2);
-//         pointValues.put(FOUR, 3);
-//         pointValues.put(FIVE, 4);
-//         pointValues.put(SIX, 5);
-//         pointValues.put(SEVEN, 6);
-//         pointValues.put(EIGHT, 7);
-//         pointValues.put(NINE, 8);
-//         pointValues.put(TEN, 9);
-//         pointValues.put(JACK, 10);
-//         pointValues.put(QUEEN, 11);
-//         pointValues.put(KING, 12);
-//         pointValues.put(ACE, 13);
-//     }
-// }
 //# sourceMappingURL=GoFish.js.map
