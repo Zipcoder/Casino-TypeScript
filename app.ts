4170341678f15c969947120b21926d8856ee7cc8
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-03-23 17:08:17 -0400

    Input working

--- app.ts
+++ app.ts
@@ -1,7 +1,19 @@
 class Startup {
+
     public static main(): void {
+
+        
+
         var webElement = document.getElementById("display");
-        webElement.innerText += "Hello World";
+        var userInput = <HTMLInputElement>document.getElementById("user_input")
+        var button = document.getElementById("submitButton");
+
+        button.addEventListener("click", (e: Event) => addToDisplayText(userInput.value))
+
+        function addToDisplayText(text: string) {
+            webElement.innerText += '\n';
+            webElement.innerText += text;
+        }
     }
 }
 
--- js/app.js
+++ js/app.js
@@ -3,7 +3,13 @@ var Startup = /** @class */ (function () {
     }
     Startup.main = function () {
         var webElement = document.getElementById("display");
-        webElement.innerText += "Hello World";
+        var userInput = document.getElementById("user_input");
+        var button = document.getElementById("submitButton");
+        button.addEventListener("click", function (e) { return addToDisplayText(userInput.value); });
+        function addToDisplayText(text) {
+            webElement.innerText += '\n';
+            webElement.innerText += text;
+        }
     };
     return Startup;
 }());
