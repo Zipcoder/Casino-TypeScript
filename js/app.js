170854784cfd6a9b6ff69bea347abd522f498aa0
Lawrence Wu <yl981@scarletmail.rutgers.edu>
2018-03-23 16:20:34 -0400

    hello world

---
+++ js/app.js
@@ -0,0 +1,12 @@
+"use strict";
+var Startup = /** @class */ (function () {
+    function Startup() {
+    }
+    Startup.main = function () {
+        var webElement = document.getElementById("display");
+        webElement.innerText += "Hello World";
+    };
+    return Startup;
+}());
+Startup.main();
+//# sourceMappingURL=app.js.map
