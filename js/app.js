fb284e8fee38ed04318d64259c0e475f0430bbd0
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-03-24 18:47:59 -0400

    js file updated with last commit

--- js/app.js
+++ js/app.js
@@ -1,20 +1,22 @@
+var webWindow = document.getElementById("display");
+var userInput = document.getElementById("user_input");
+var button = document.getElementById("submitButton");
+webWindow.innerText += "Welcome to the worst casino you've ever seen!";
+function addToDisplayText(text) {
+    webWindow.innerText += '\n';
+    webWindow.innerText += text;
+}
 var Startup = /** @class */ (function () {
     function Startup() {
     }
     Startup.main = function () {
-        var webElement = document.getElementById("display");
-        var userInput = document.getElementById("user_input");
-        var button = document.getElementById("submitButton");
-        webElement.innerText += "Welcome to the worst casino you've ever seen!";
+        // var webWindow = document.getElementById("display");
+        // var userInput = <HTMLInputElement>document.getElementById("user_input");
+        // var button = document.getElementById("submitButton");
         button.addEventListener("click", function (e) { return addToDisplayText(userInput.value); });
-        function addToDisplayText(text) {
-            webElement.innerText += '\n';
-            webElement.innerText += text;
-        }
     };
     return Startup;
 }());
-Startup.main();
 var Profile = /** @class */ (function () {
     function Profile(name, balance) {
         this.id = 1;
@@ -29,11 +31,13 @@ var Profile = /** @class */ (function () {
 var SlotMachine = /** @class */ (function () {
     function SlotMachine() {
     }
+    SlotMachine.prototype.start = function () {
+        var firstReel = Math.floor(Math.random() * 5) + 1;
+        var secondReel = Math.floor(Math.random() * 5) + 1;
+        var thirdReel = Math.floor(Math.random() * 5) + 1;
+        // Implement gambling interface and add betting too
+    };
     return SlotMachine;
 }());
-var SlotMachineReel = /** @class */ (function () {
-    function SlotMachineReel() {
-    }
-    return SlotMachineReel;
-}());
+Startup.main();
 //# sourceMappingURL=app.js.map
