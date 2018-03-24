c977f38110888690af5ad1837222bc2fad7eeac3
Anthony Jordan <Kain235@gmail.com>
2018-03-24 16:16:56 -0400

    Testing

--- js/app.ts
+++ js/app.ts
@@ -1,1 +1,8 @@
-console.log("Hello")
+document.getElementById("submit_button").addEventListener("click", testFunction);
+document.getElementById("display").addEventListener("mouseover", testFunction2); 
+function testFunction(){
+    document.getElementById("display").innerHTML = "Hello";
+}
+ function testFunction2(){
+     document.getElementById("display").innerHTML = "Press the button to start";
+ }
