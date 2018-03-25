5b48eaf154537821f6cdf2f42e4fe3c7af3dc65e
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-03-24 21:02:15 -0400

    finally got that damn button working

--- js/app.js
+++ js/app.js
@@ -3,12 +3,86 @@ var Startup = /** @class */ (function () {
     function Startup() {
     }
     Startup.main = function () {
-        var input = document.getElementById("user_input");
-        var window = document.getElementById('display');
-        var button = document.getElementById('submit');
-        button.addEventListener("click", function (e) { window.innerText += input.value + '\n'; });
-        button.addEventListener("click", function (e) { input.value = ''; });
+        var casino = new Casino();
+        casino.start();
     };
     return Startup;
 }());
+var Casino = /** @class */ (function () {
+    function Casino() {
+        this.chooser = this.chooseGame.bind(this);
+    }
+    Casino.prototype.start = function () {
+        UI.display("What game do you want to play?");
+        UI.display("Black Jack or Go Fish?");
+        UI.button.addEventListener("click", this.chooser);
+    };
+    Casino.prototype.chooseGame = function () {
+        UI.button.removeEventListener("click", this.chooser);
+        if (UI.lastInput === "Black Jack") {
+            BlackJack.start();
+        }
+        else if (UI.lastInput === "Go Fish") {
+            GoFish.start();
+        }
+        else {
+            UI.button.addEventListener("click", this.chooser);
+        }
+    };
+    return Casino;
+}());
+var UI = /** @class */ (function () {
+    function UI() {
+        UI.button.addEventListener("click", function (e) { UI._lastInput = UI.userInput.value; });
+        UI.button.addEventListener("click", function (e) { UI.userInput.value = ''; });
+    }
+    UI.display = function (input) {
+        this.window.innerText += input + '\n';
+    };
+    UI.clearScreen = function () {
+        this.window.innerText = '';
+    };
+    UI.getInput = function () {
+        UI.button.addEventListener("click", function (e) { UI._lastInput = UI.userInput.value; });
+        UI.button.addEventListener("click", function (e) { UI.userInput.value = ''; });
+    };
+    Object.defineProperty(UI, "Instance", {
+        get: function () {
+            return this._instance || (this._instance = new UI());
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(UI, "lastInput", {
+        get: function () {
+            return this._lastInput;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    UI.userInput = document.getElementById("user_input");
+    UI.window = document.getElementById('display');
+    UI.button = document.getElementById('submit');
+    return UI;
+}());
+var BlackJack = /** @class */ (function () {
+    function BlackJack() {
+    }
+    BlackJack.start = function () {
+        UI.clearScreen();
+        UI.display("Black Jack");
+    };
+    return BlackJack;
+}());
+var GoFish = /** @class */ (function () {
+    function GoFish() {
+    }
+    GoFish.start = function () {
+        UI.clearScreen();
+        UI.display("Go Fish");
+    };
+    return GoFish;
+}());
+//var lastInput : any;    
+var UIInstance = UI.Instance;
 Startup.main();
--- js/app.ts
+++ js/app.ts
@@ -1,14 +1,99 @@
 class Startup {
-   
+
     public static main(): void {
-        var input = <HTMLInputElement> document.getElementById("user_input");
-        var window = <HTMLDivElement>document.getElementById('display');
-        var button = <HTMLDivElement>document.getElementById('submit');
-        
-        button.addEventListener("click",(e:Event)=>{window.innerText += input.value + '\n'});
-        button.addEventListener("click",(e:Event)=>{input.value = ''});
- 
+        var casino = new Casino();
+        casino.start();
     }
 }
 
+
+class Casino {
+
+    constructor(){}
+
+    start() {
+        UI.display("What game do you want to play?");
+        UI.display("Black Jack or Go Fish?");
+        UI.button.addEventListener("click", this.chooser);
+    }
+
+     chooseGame(): void {
+        UI.button.removeEventListener("click", this.chooser);
+        if (UI.lastInput === "Black Jack") {
+            
+            BlackJack.start();
+    
+        }
+        else if (UI.lastInput === "Go Fish") {
+            GoFish.start();
+    
+        }
+        else {
+            UI.button.addEventListener("click", this.chooser);
+        }
+    }
+
+    chooser = this.chooseGame.bind(this);
+
+
+
+}
+
+
+
+
+class UI {
+    static userInput = <HTMLInputElement>document.getElementById("user_input");
+    static window = <HTMLDivElement>document.getElementById('display');
+    static button = <HTMLDivElement>document.getElementById('submit');
+    static _lastInput: any;
+    private static _instance: UI;
+
+    private constructor() {
+        UI.button.addEventListener("click", (e: Event) => { UI._lastInput = UI.userInput.value });
+        UI.button.addEventListener("click", (e: Event) => { UI.userInput.value = '' });
+
+    }
+
+    static display(input: string): void {
+        this.window.innerText += input + '\n';
+    }
+
+    static clearScreen(): void {
+        this.window.innerText = '';
+    }
+
+    static getInput(): void {
+        UI.button.addEventListener("click", (e: Event) => { UI._lastInput = UI.userInput.value });
+        UI.button.addEventListener("click", (e: Event) => { UI.userInput.value = '' });
+
+    }
+
+    public static get Instance(): UI {
+        return this._instance || (this._instance = new UI());
+    }
+
+    public static get lastInput(): any {
+        return this._lastInput;
+
+    }
+
+}
+
+class BlackJack {
+    static start(): void {
+        UI.clearScreen();
+        UI.display("Black Jack");
+    }
+}
+
+class GoFish {
+    static start(): void {
+        UI.clearScreen();
+        UI.display("Go Fish");
+    }
+}
+
+//var lastInput : any;    
+const UIInstance = UI.Instance;
 Startup.main();
