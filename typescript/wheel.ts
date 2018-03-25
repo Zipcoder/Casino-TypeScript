0e69b9fc073c480fa120f74f632506438fe462ad
Kaitrina High <kaitrinahigh@zipcoders-MacBook-Pro-81.local>
2018-03-24 20:01:38 -0400

    Err,didn't turn on autoSave in IDE

---
+++ typescript/wheel.ts
@@ -0,0 +1,11 @@
+class Wheel{
+
+interval: Number;
+
+constructor(interval: Number){
+    this.interval = interval;
+}
+
+
+
+}
---
+++ typescript/wheel_symbol.ts
@@ -0,0 +1,30 @@
+enum WheelSymbol{
+Heart,
+Club,
+Spade,
+Diamond,
+Cherry,
+}
+
+const WheelSymbolSeed: WheelSymbol[] = [
+    WheelSymbol.Diamond,
+    WheelSymbol.Diamond,
+    WheelSymbol.Diamond,
+    WheelSymbol.Diamond,
+    WheelSymbol.Diamond,
+    WheelSymbol.Diamond,
+    WheelSymbol.Heart,
+    WheelSymbol.Heart,
+    WheelSymbol.Heart,
+    WheelSymbol.Heart,
+    WheelSymbol.Heart,
+    WheelSymbol.Heart,
+    WheelSymbol.Spade,
+    WheelSymbol.Spade,
+    WheelSymbol.Spade,
+    WheelSymbol.Spade,
+    WheelSymbol.Club,
+    WheelSymbol.Club,
+    WheelSymbol.Club,
+    WheelSymbol.Cherry,
+]
