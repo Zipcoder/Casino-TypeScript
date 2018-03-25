b0ad1fd9225d69a53b3a9594c1ceb45d128f5d7b
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-03-25 14:07:43 -0400

    Structure progress, casino built, just need slot machine logic

--- app.ts
+++ app.ts
@@ -2,33 +2,24 @@ var webWindow = document.getElementById("display");
 var userInput = <HTMLInputElement>document.getElementById("user_input");
 var button = document.getElementById("submitButton");
 
-webWindow.innerText += "Welcome to the worst casino you've ever seen!";
-
 function addToDisplayText(text: string) {
     webWindow.innerText += '\n';
     webWindow.innerText += text;
 }
 
-class Startup {
-    public static main(): void {
-        // var webWindow = document.getElementById("display");
-        // var userInput = <HTMLInputElement>document.getElementById("user_input");
-        // var button = document.getElementById("submitButton");
-
-        button.addEventListener("click", (e: Event) => addToDisplayText(userInput.value));
-    }
-}
-
 class Profile {
     private id: number = 1;
-    private name: string;
-    private balance: number;
-    constructor(name: string, balance: number) {
-        this.name = name;
-        this.balance = balance;
-    }
-    public getBalance(){
+    private _name: string = null;
+    private _balance: number = 0;
+    constructor() {}
+    get balance(): number {
         return this.balance;
+    }
+    set balance(theBalance: number) {
+        this.balance = theBalance;
+    }
+    set name(theName: string) {
+        this._name = theName;
     }
 }
 
@@ -40,13 +31,49 @@ interface GamblingInterface {
     bet();
 }
 
+// Player initialization had to be removed to here because this language is bleh
+var player:Profile = null;
+
 class SlotMachine implements GameInterface{
     start() {
-        var firstReel = Math.floor(Math.random() * 5) + 1;
-        var secondReel = Math.floor(Math.random() * 5) + 1;
-        var thirdReel = Math.floor(Math.random() * 5) + 1;
-        // Implement gambling interface and add betting too
+        addToDisplayText("Welcome to slot machine! Win triple your bet for 3 matching numbers or 1.5x for 2.");
+        while(true) {
+            var currentInput;
+            var currentBalance = player.balance;
+            addToDisplayText("You have $" + currentBalance + ". Enter a number less than your total to bet.");
+            addToDisplayText("Enter anything else to quit.");
+            button.addEventListener("click", (e: Event) => currentInput = userInput.value);
+            addToDisplayText("You entered " + currentInput + ".");
+            if ((!isNaN(currentInput)) && (currentInput <= currentBalance)) {
+                addToDisplayText("Valid input! Spinning reels...");
+                var firstReel = Math.floor(Math.random() * 5) + 1;
+                var secondReel = Math.floor(Math.random() * 5) + 1;
+                var thirdReel = Math.floor(Math.random() * 5) + 1;
+
+            // More logic please
+            } else {
+                addToDisplayText("Invalid input! Bye-bye!");
+                break;
+            }
+        }
     }
 }
 
+class Startup {
+    public static main(): void {
+        addToDisplayText("Please enter your name:");
+        button.addEventListener("click", (e: Event) => player.name = userInput.value);
+        addToDisplayText("Please enter how many dollary doos you want to start with:")
+        button.addEventListener("click", (e: Event) => player.balance = +userInput.value);
+        addToDisplayText("My girlfriend cut her hand and we spent all weekend in the hospital so there is only a slot machine lol");
+        
+    }
+}
+
+webWindow.innerText = "Welcome to the worst casino you've ever seen!";
 Startup.main();
+
+
+
+// Here for posterity
+// button.addEventListener("click", (e: Event) => addToDisplayText(userInput.value));
--- js/app.js
+++ js/app.js
@@ -1,43 +1,78 @@
 var webWindow = document.getElementById("display");
 var userInput = document.getElementById("user_input");
 var button = document.getElementById("submitButton");
-webWindow.innerText += "Welcome to the worst casino you've ever seen!";
 function addToDisplayText(text) {
     webWindow.innerText += '\n';
     webWindow.innerText += text;
 }
-var Startup = /** @class */ (function () {
-    function Startup() {
-    }
-    Startup.main = function () {
-        // var webWindow = document.getElementById("display");
-        // var userInput = <HTMLInputElement>document.getElementById("user_input");
-        // var button = document.getElementById("submitButton");
-        button.addEventListener("click", function (e) { return addToDisplayText(userInput.value); });
-    };
-    return Startup;
-}());
 var Profile = /** @class */ (function () {
-    function Profile(name, balance) {
+    function Profile() {
         this.id = 1;
-        this.name = name;
-        this.balance = balance;
+        this._name = null;
+        this._balance = 0;
     }
-    Profile.prototype.getBalance = function () {
-        return this.balance;
-    };
+    Object.defineProperty(Profile.prototype, "balance", {
+        get: function () {
+            return this.balance;
+        },
+        set: function (theBalance) {
+            this.balance = theBalance;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Profile.prototype, "name", {
+        set: function (theName) {
+            this._name = theName;
+        },
+        enumerable: true,
+        configurable: true
+    });
     return Profile;
 }());
+// Player initialization had to be removed to here because this language is bleh
+var player = null;
 var SlotMachine = /** @class */ (function () {
     function SlotMachine() {
     }
     SlotMachine.prototype.start = function () {
-        var firstReel = Math.floor(Math.random() * 5) + 1;
-        var secondReel = Math.floor(Math.random() * 5) + 1;
-        var thirdReel = Math.floor(Math.random() * 5) + 1;
-        // Implement gambling interface and add betting too
+        addToDisplayText("Welcome to slot machine! Win triple your bet for 3 matching numbers or 1.5x for 2.");
+        while (true) {
+            var currentInput;
+            var currentBalance = player.balance;
+            addToDisplayText("You have $" + currentBalance + ". Enter a number less than your total to bet.");
+            addToDisplayText("Enter anything else to quit.");
+            button.addEventListener("click", function (e) { return currentInput = userInput.value; });
+            addToDisplayText("You entered " + currentInput + ".");
+            if ((!isNaN(currentInput)) && (currentInput <= currentBalance)) {
+                addToDisplayText("Valid input! Spinning reels...");
+                var firstReel = Math.floor(Math.random() * 5) + 1;
+                var secondReel = Math.floor(Math.random() * 5) + 1;
+                var thirdReel = Math.floor(Math.random() * 5) + 1;
+                // More logic please
+            }
+            else {
+                addToDisplayText("Invalid input! Bye-bye!");
+                break;
+            }
+        }
     };
     return SlotMachine;
 }());
+var Startup = /** @class */ (function () {
+    function Startup() {
+    }
+    Startup.main = function () {
+        addToDisplayText("Please enter your name:");
+        button.addEventListener("click", function (e) { return player.name = userInput.value; });
+        addToDisplayText("Please enter how many dollary doos you want to start with:");
+        button.addEventListener("click", function (e) { return player.balance = +userInput.value; });
+        addToDisplayText("My girlfriend cut her hand and we spent all weekend in the hospital so there is only a slot machine lol");
+    };
+    return Startup;
+}());
+webWindow.innerText = "Welcome to the worst casino you've ever seen!";
 Startup.main();
+// Here for posterity
+// button.addEventListener("click", (e: Event) => addToDisplayText(userInput.value));
 //# sourceMappingURL=app.js.map
