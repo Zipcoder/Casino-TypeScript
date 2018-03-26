e8a5d7865f4f6814233fb255bedaa8f0301ff662
ahsonali <ahsonac@gmail.com>
2018-03-25 21:11:41 -0400

    submit button works

---
+++ app/Profile.js
@@ -0,0 +1,8 @@
+var Profile = /** @class */ (function () {
+    function Profile(id, name, balance) {
+        this.balance = balance;
+        this.id = id;
+        this.name = name;
+    }
+    return Profile;
+}());
