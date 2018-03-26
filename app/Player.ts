ba7c4bd37a9b797b238d34d84b117154e3365098
ghost1497 <rfrankie04@gmail.com>
2018-03-25 22:10:48 -0400

    part 1 complete

---
+++ app/Player.ts
@@ -0,0 +1,22 @@
+class Player implements PlayerInterface {
+    
+    id: number;
+    name: string;
+    profile: Profile;
+    constructor(id: number, name: string, balance: number){
+    this.name = name;
+    this.id = id;
+    this.profile =  new Profile(this.id, this.name, balance);
+    }
+
+
+    getProfile(): Profile {
+        return this.profile;
+    }
+    getName(): String {
+        return this.name;
+    }
+    getId(): number {
+        return this.id;
+    }
+}
