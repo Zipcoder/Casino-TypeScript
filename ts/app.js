ade36b57dbaafbdab257e1f5cdd4867ac3436727
Luis Romero <luisromero@zipcoders-MacBook-Pro-112.local>
2018-03-26 09:22:38 -0400

    I made a deck (exclamation mark)

--- ts/app.js
+++ ts/app.js
@@ -9,15 +9,9 @@ var UserInterface = /** @class */ (function () {
         this.button.addEventListener("click", function (e) { _this.userInput.value = ''; });
         // this.chooseGame = this.chooseGame.bind(this);
     }
-    UserInterface.prototype.display = function (input) {
-        this.window.innerText += input + '\n';
-    };
-    UserInterface.prototype.clearScreen = function () {
-        this.window.innerText = '';
-    };
-    UserInterface.prototype.lastInput = function () {
-        return this._lastInput;
-    };
+    UserInterface.prototype.display = function (input) { this.window.innerText += input + '\n'; };
+    UserInterface.prototype.clearScreen = function () { this.window.innerText = ''; };
+    UserInterface.prototype.lastInput = function () { return this._lastInput; };
     UserInterface.prototype.start = function () {
         var _this = this;
         this.display("Do you want to play Blackjack? (y/n)");
@@ -46,9 +40,7 @@ var UserInterface = /** @class */ (function () {
 var MathOps = /** @class */ (function () {
     function MathOps() {
     }
-    MathOps.sum = function (number1, number2) {
-        return (number1 + number2);
-    };
+    MathOps.sum = function (number1, number2) { return (number1 + number2); };
     return MathOps;
 }());
 var RankNumber = /** @class */ (function () {
@@ -60,9 +52,20 @@ var RankNumber = /** @class */ (function () {
         }
         return this._INSTANCE;
     };
-    RankNumber.getRankNumbers = function () {
-        return this.rankNumbers;
-    };
+    RankNumber.getRankNumbers = function () { return this.rankNumbers; };
+    RankNumber.DEUCE = function () { return this.rankNumbers[0]; };
+    RankNumber.THREE = function () { return this.rankNumbers[1]; };
+    RankNumber.FOUR = function () { return this.rankNumbers[2]; };
+    RankNumber.FIVE = function () { return this.rankNumbers[3]; };
+    RankNumber.SIX = function () { return this.rankNumbers[4]; };
+    RankNumber.SEVEN = function () { return this.rankNumbers[5]; };
+    RankNumber.EIGHT = function () { return this.rankNumbers[6]; };
+    RankNumber.NINE = function () { return this.rankNumbers[7]; };
+    RankNumber.TEN = function () { return this.rankNumbers[8]; };
+    RankNumber.JACK = function () { return this.rankNumbers[9]; };
+    RankNumber.QUEEN = function () { return this.rankNumbers[10]; };
+    RankNumber.KING = function () { return this.rankNumbers[11]; };
+    RankNumber.ACE = function () { return this.rankNumbers[12]; };
     RankNumber.rankNumbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1];
     return RankNumber;
 }());
@@ -75,9 +78,20 @@ var RankString = /** @class */ (function () {
         }
         return this._INSTANCE;
     };
-    RankString.getRankStrings = function () {
-        return this.rankStrings;
-    };
+    RankString.getRankStrings = function () { return this.rankStrings; };
+    RankString.DEUCE = function () { return this.rankStrings[0]; };
+    RankString.THREE = function () { return this.rankStrings[1]; };
+    RankString.FOUR = function () { return this.rankStrings[2]; };
+    RankString.FIVE = function () { return this.rankStrings[3]; };
+    RankString.SIX = function () { return this.rankStrings[4]; };
+    RankString.SEVEN = function () { return this.rankStrings[5]; };
+    RankString.EIGHT = function () { return this.rankStrings[6]; };
+    RankString.NINE = function () { return this.rankStrings[7]; };
+    RankString.TEN = function () { return this.rankStrings[8]; };
+    RankString.JACK = function () { return this.rankStrings[9]; };
+    RankString.QUEEN = function () { return this.rankStrings[10]; };
+    RankString.KING = function () { return this.rankStrings[11]; };
+    RankString.ACE = function () { return this.rankStrings[12]; };
     RankString.rankStrings = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
     return RankString;
 }());
@@ -90,9 +104,11 @@ var SuitString = /** @class */ (function () {
         }
         return this._INSTANCE;
     };
-    SuitString.getRankStrings = function () {
-        return this.suitStrings;
-    };
+    SuitString.getSuitStrings = function () { return this.suitStrings; };
+    SuitString.CLUBS = function () { return this.suitStrings[0]; };
+    SuitString.DIAMONDS = function () { return this.suitStrings[1]; };
+    SuitString.HEARTS = function () { return this.suitStrings[2]; };
+    SuitString.SPADES = function () { return this.suitStrings[3]; };
     SuitString.suitStrings = ["clubs", "diamonds", "hearts", "spades"];
     return SuitString;
 }());
@@ -105,56 +121,44 @@ var SuitSymbol = /** @class */ (function () {
         }
         return this._INSTANCE;
     };
-    SuitSymbol.getSuitSymbols = function () {
-        return this.suitSymbols;
-    };
+    SuitSymbol.getSuitSymbols = function () { return this.suitSymbols; };
+    SuitSymbol.CLUBS = function () { return this.suitSymbols[0]; };
+    SuitSymbol.DIAMONDS = function () { return this.suitSymbols[1]; };
+    SuitSymbol.HEARTS = function () { return this.suitSymbols[2]; };
+    SuitSymbol.SPADES = function () { return this.suitSymbols[3]; };
     SuitSymbol.suitSymbols = ["\u2663", "\u2666", "\u2665", "\u2660"];
     return SuitSymbol;
 }());
-// class Card {
-//     private rankNumber: RankNumber;
-//     private rankString: RankString;
-//     private suitSymbol: SuitSymbol;
-//     constructor(rankNumber: RankNumber, rankString: RankString, suitSymbol: SuitSymbol) {
-//         this.rankNumber = rankNumber;
-//         this.rankString = rankString;
-//         this.suitSymbol = suitSymbol;
-//     }
-//     getRankNumber(): number {
-//         return this.rankNumber;
-//     }
-//     getRankString(): string {
-//         return this.rankString;
-//     }
-//     getSuitSymbol(): string {
-//         return this.suitSymbol;
-//     }
-//     toString(): string {
-//         return this.rankString + this.suitSymbol;
-//     }
-// }
-// class Deck {
-//     private deck: Card[];
-//     private rankNumbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1];
-//     private rankStrings = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
-//     private suitSymbols = ["\u2663", "\u2666", "\u2665", "\u2660"];
-//     constructor() {
-//         this.deck = [];
-//         for (let i = 0; i < 4; i++) {
-//             for (let j = 0; j < 13; j++) {
-//                 this.deck.push(new Card(this.rankNumbers[j], this.rankStrings[j], this.suitSymbols[i]);
-//             }
-//         }
-//     }
-//     getDeck(): Card[] {
-//         return this.deck;
-//     }
-// }
+var Card = /** @class */ (function () {
+    function Card(rankNumber, rankString, suitSymbol, suitString) {
+        this.rankNumber = rankNumber;
+        this.rankString = rankString;
+        this.suitSymbol = suitSymbol;
+        this.suitString = suitString;
+    }
+    Card.prototype.getRankNumber = function () { return this.rankNumber; };
+    Card.prototype.getRankString = function () { return this.rankString; };
+    Card.prototype.getSuitSymbol = function () { return this.suitSymbol; };
+    Card.prototype.getSuitString = function () { return this.suitString; };
+    Card.prototype.toString = function () { return this.rankString + this.suitSymbol; };
+    return Card;
+}());
+var Deck = /** @class */ (function () {
+    function Deck() {
+        this.deck = [];
+        for (var i = 0; i < 4; i++) {
+            for (var j = 0; j < 13; j++) {
+                this.deck.push(new Card(RankNumber.getRankNumbers()[j], RankString.getRankStrings()[j], SuitSymbol.getSuitSymbols()[i], SuitString.getSuitStrings()[i]));
+            }
+        }
+    }
+    Deck.prototype.getDeck = function () { return this.deck; };
+    return Deck;
+}());
 var UI = new UserInterface();
-// let card: Card = new Card(RankNumber.ACE, RankString.ACE, SuitSymbol.HEARTS);
-// let deck: Deck = new Deck();
+var card = new Card(RankNumber.KING(), RankString.KING(), SuitSymbol.SPADES(), SuitString.SPADES());
+var deck = new Deck();
 UI.start();
-var myNum = RankNumber.getRankNumbers()[12];
-UI.display(RankNumber.getRankNumbers()[0]);
-UI.display(myNum);
-// UI.display(card.toString());
+UI.display(RankNumber.DEUCE());
+UI.display(card.toString());
+UI.display(deck.getDeck()[12]);
--- ts/app.ts
+++ ts/app.ts
@@ -11,17 +11,9 @@ class UserInterface {
         // this.chooseGame = this.chooseGame.bind(this);
     }
 
-    display(input: any): void {
-        this.window.innerText += input + '\n';
-    }
-
-    clearScreen(): void {
-        this.window.innerText = '';
-    }
-
-    lastInput(): any {
-        return this._lastInput;
-    }
+    display(input: any): void { this.window.innerText += input + '\n'; }
+    clearScreen(): void { this.window.innerText = ''; }
+    lastInput(): any { return this._lastInput; }
 
     start() {
         this.display("Do you want to play Blackjack? (y/n)");
@@ -45,149 +37,151 @@ class UserInterface {
             // this.display("Please try again");
         }
     }
-
 }
 
 class MathOps {
-    constructor() {
-    }
-
-    public static sum(number1: number, number2: number): number {
-        return (number1 + number2);
-    }
+    constructor() {}
+    public static sum(number1: number, number2: number): number { return (number1 + number2); }
 }
 
 class RankNumber {
     private static _INSTANCE: RankNumber;
     private static rankNumbers: number[] = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1];
 
-    private constructor() {
-    }
+    private constructor() {}
 
     public static getInstance(): RankNumber {
-        if (this._INSTANCE === null) {
-            this._INSTANCE = new RankNumber();
-        }
+        if (this._INSTANCE === null) { this._INSTANCE = new RankNumber(); }
         return this._INSTANCE;
     } 
 
-    public static getRankNumbers(): number[] {
-        return this.rankNumbers;
-    }
+    public static getRankNumbers(): number[] { return this.rankNumbers; }
+
+    public static DEUCE(): number { return this.rankNumbers[0]; }
+    public static THREE(): number { return this.rankNumbers[1]; }
+    public static FOUR(): number { return this.rankNumbers[2]; } 
+    public static FIVE(): number { return this.rankNumbers[3]; }
+    public static SIX(): number { return this.rankNumbers[4]; }
+    public static SEVEN(): number { return this.rankNumbers[5]; }
+    public static EIGHT(): number { return this.rankNumbers[6]; }
+    public static NINE(): number { return this.rankNumbers[7]; }
+    public static TEN(): number { return this.rankNumbers[8]; }
+    public static JACK(): number { return this.rankNumbers[9]; }
+    public static QUEEN(): number { return this.rankNumbers[10]; }
+    public static KING(): number { return this.rankNumbers[11]; }
+    public static ACE(): number { return this.rankNumbers[12]; }
 }
 
 class RankString {
     private static _INSTANCE: RankString;
     private static rankStrings: string[] = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 
-    private constructor() {
-    }
+    private constructor() {}
 
     public static getInstance(): RankString {
-        if (this._INSTANCE === null) {
-            this._INSTANCE = new RankString();
-        }
+        if (this._INSTANCE === null) { this._INSTANCE = new RankString(); }
         return this._INSTANCE;
     }
 
-    public static getRankStrings(): string[] {
-        return this.rankStrings;
-    }
+    public static getRankStrings(): string[] { return this.rankStrings; }
+
+    public static DEUCE(): string { return this.rankStrings[0]; }
+    public static THREE(): string { return this.rankStrings[1]; }
+    public static FOUR(): string { return this.rankStrings[2]; } 
+    public static FIVE(): string { return this.rankStrings[3]; }
+    public static SIX(): string { return this.rankStrings[4]; }
+    public static SEVEN(): string { return this.rankStrings[5]; }
+    public static EIGHT(): string { return this.rankStrings[6]; }
+    public static NINE(): string { return this.rankStrings[7]; }
+    public static TEN(): string { return this.rankStrings[8]; }
+    public static JACK(): string { return this.rankStrings[9]; }
+    public static QUEEN(): string { return this.rankStrings[10]; }
+    public static KING(): string { return this.rankStrings[11]; }
+    public static ACE(): string { return this.rankStrings[12]; }
 }
 
 class SuitString {
     private static _INSTANCE: SuitString;
     private static suitStrings: string[] = ["clubs", "diamonds", "hearts", "spades"];
 
-    private constructor() {
-    }
+    private constructor() {}
 
     public static getInstance() {
-        if (this._INSTANCE === null) {
-            this._INSTANCE = new SuitString();
-        }
+        if (this._INSTANCE === null) { this._INSTANCE = new SuitString(); }
         return this._INSTANCE;
     }
 
-    public static getRankStrings(): string[] {
-        return this.suitStrings;
-    }
+    public static getSuitStrings(): string[] { return this.suitStrings; }
+
+    public static CLUBS(): string { return this.suitStrings[0]; }
+    public static DIAMONDS(): string { return this.suitStrings[1]; }
+    public static HEARTS(): string { return this.suitStrings[2]; } 
+    public static SPADES(): string { return this.suitStrings[3]; }
 }
 
 class SuitSymbol {
     private static _INSTANCE: SuitSymbol;
     private static suitSymbols: string[] = ["\u2663", "\u2666", "\u2665", "\u2660"];
 
-    private constructor() {
-    }
+    private constructor() {}
 
     public static getInstance() {
-        if (this._INSTANCE === null) {
-            this._INSTANCE = new SuitSymbol();
-        }
+        if (this._INSTANCE === null) { this._INSTANCE = new SuitSymbol(); }
         return this._INSTANCE;
     }
 
-    public static getSuitSymbols(): string[] {
-        return this.suitSymbols;
-    }
+    public static getSuitSymbols(): string[] { return this.suitSymbols; }
+
+    public static CLUBS(): string { return this.suitSymbols[0]; }
+    public static DIAMONDS(): string { return this.suitSymbols[1]; }
+    public static HEARTS(): string { return this.suitSymbols[2]; } 
+    public static SPADES(): string { return this.suitSymbols[3]; }
 }
 
-// class Card {
-//     private rankNumber: RankNumber;
-//     private rankString: RankString;
-//     private suitSymbol: SuitSymbol;
+class Card {
+    private rankNumber: number;
+    private rankString: string;
+    private suitSymbol: string;
+    private suitString: string;
 
-//     constructor(rankNumber: RankNumber, rankString: RankString, suitSymbol: SuitSymbol) {
-//         this.rankNumber = rankNumber;
-//         this.rankString = rankString;
-//         this.suitSymbol = suitSymbol;
-//     }
+    constructor(rankNumber: number, rankString: string, suitSymbol: string, suitString: string) {
+        this.rankNumber = rankNumber;
+        this.rankString = rankString;
+        this.suitSymbol = suitSymbol;
+        this.suitString = suitString;
+    }
 
-//     getRankNumber(): number {
-//         return this.rankNumber;
-//     }
+    getRankNumber(): number { return this.rankNumber; }
+    getRankString(): string { return this.rankString; }
+    getSuitSymbol(): string { return this.suitSymbol; }
+    getSuitString(): string { return this.suitString; }
+    toString(): string { return this.rankString + this.suitSymbol; }
+}
 
-//     getRankString(): string {
-//         return this.rankString;
-//     }
+class Deck {
+    private deck: Card[];
 
-//     getSuitSymbol(): string {
-//         return this.suitSymbol;
-//     }
+    constructor() {
+        this.deck = [];
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 13; j++) {
+                this.deck.push(new Card(RankNumber.getRankNumbers()[j], 
+                                        RankString.getRankStrings()[j],
+                                        SuitSymbol.getSuitSymbols()[i], 
+                                        SuitString.getSuitStrings()[i]));
+            }
+        }
+    }
 
-//     toString(): string {
-//         return this.rankString + this.suitSymbol;
-//     }
-// }
-
-// class Deck {
-//     private deck: Card[];
-//     private rankNumbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1];
-//     private rankStrings = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
-//     private suitSymbols = ["\u2663", "\u2666", "\u2665", "\u2660"];
-
-//     constructor() {
-//         this.deck = [];
-//         for (let i = 0; i < 4; i++) {
-//             for (let j = 0; j < 13; j++) {
-//                 this.deck.push(new Card(this.rankNumbers[j], this.rankStrings[j], this.suitSymbols[i]);
-//             }
-//         }
-//     }
-
-//     getDeck(): Card[] {
-//         return this.deck;
-//     }
-// }
+    getDeck(): Card[] { return this.deck; }
+}
 
 let UI: UserInterface = new UserInterface();
-// let card: Card = new Card(RankNumber.ACE, RankString.ACE, SuitSymbol.HEARTS);
-// let deck: Deck = new Deck();
+let card: Card = new Card(RankNumber.KING(), RankString.KING(), SuitSymbol.SPADES(), SuitString.SPADES());
+let deck: Deck = new Deck();
 
 UI.start();
-let myNum: number = RankNumber.getRankNumbers()[12];
-UI.display(RankNumber.getRankNumbers()[0]);
-UI.display(myNum);
-// UI.display(card.toString());
+UI.display(RankNumber.DEUCE());
+UI.display(card.toString());
+UI.display(deck.getDeck()[12]);
 
